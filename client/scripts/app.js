4db1596b3939112c889c695665004632483b2fbb
Max Kahn <kahn.max@gmail.com>
2015-12-09 11:32:59 -0800

    Passes unit tests for model part.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -15,14 +15,21 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', function() {
+      this.sort();
+    });
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
+
+  // this.on('change', function() {
+  //   this.sort();
+  // }, this);
 
 });
 
